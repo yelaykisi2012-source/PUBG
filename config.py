767b6e7700cbcfbcf2dba332c8a1262169e97899
yelaykisi2012-source <yelaykisi2012@gmail.com>
2026-03-01 14:57:52 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = "8203964385:AAFPPF8KoMwkqukKj_6MIGQ43lekOgNirUY"
+ADMIN_IDS = [8348353169, 7698488373]  # 2 ta admin
+
+CARD_NUMBER = "9860 0803 0993 7706"
+CARD_NAME = "Tursunboyeva.F"
+PHONE_NUMBER = "97 467 07 10"
+PRICE = "40 000 so'm"
